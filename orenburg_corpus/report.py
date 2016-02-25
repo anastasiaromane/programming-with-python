954e403a065b46d90f22a9e8e9874f883985942d
anastasiaromane <anastasiaromane@gmail.com>
2016-02-25 13:44:35 +0300

    changing functions

--- orenburg_corpus/report.py
+++ orenburg_corpus/report.py
@@ -5,75 +5,74 @@ from datetime import date
 from settings import REPORT_FILE
 
 
-class Report(object):
-    """Формирование файла отчёта"""
+SPHERE = "публицистика"
+TOPIC = ""
+STYLE = "нейтральный"
+AUDIENCE_AGE = "н-возраст"
+AUDIENCE_LEVEL = "н-уровень"
+AUDIENCE_SIZE = "городская"
+MEDIUM = "газета"
+COUNTRY = "Россия"
+REGION = "Оренбург"
+LANGUAGE = "ru"
+PUBLICATION = "Вечерний Оренбург"
 
-    SPHERE = "публицистика"
-    TOPIC = ""
-    STYLE = "нейтральный"
-    AUDIENCE_AGE = "н-возраст"
-    AUDIENCE_LEVEL = "н-уровень"
-    AUDIENCE_SIZE = "городская"
-    MEDIUM = "газета"
-    COUNTRY = "Россия"
-    REGION = "Оренбург"
-    LANGUAGE = "ru"
-    PUBLICATION = "Вечерний Оренбург"
+report_file = open(REPORT_FILE, 'a', newline='', encoding='windows-1251')
 
-    def __init__(self):
-        self.report_file = open(REPORT_FILE, 'a', newline='', encoding='windows-1251')
-        self.report = csv.writer(
-            self.report_file, delimiter=";", quotechar='"',
-            quoting=csv.QUOTE_ALL
-        )
+report = csv.writer(
+    report_file, delimiter=";", quotechar='"',
+    quoting=csv.QUOTE_ALL
+)
 
-        # head
-        self.report.writerow([
-            'path', 'author', 'sex', 'birthday', 'header', 'created', 'sphere',
-            'genre_fi', 'type', 'topic', 'chronotop', 'style', 'audience_age',
-            'audience_level', 'audience_size', 'source', 'publication',
-            'publisher', 'publ_year', 'medium', 'country', 'region', 'language',
-        ])
 
-    def write(self, path, header, created, source, author=''):
-        """ Пишет в файл отчёта строку с данными в кодировке windows-1251
+def header():
+    report.writerow([
+        'path', 'author', 'sex', 'birthday', 'header', 'created', 'sphere',
+        'genre_fi', 'type', 'topic', 'chronotop', 'style', 'audience_age',
+        'audience_level', 'audience_size', 'source', 'publication',
+        'publisher', 'publ_year', 'medium', 'country', 'region', 'language',
+    ])
 
-        Args:
-            path (unicode):  путь к файлу со статьёй
-            header (unicode): название статьи
-            created (datetime.date): дата публикации
-            source (unicode): URL, откуда статья была скачана
-            author (unicode): имя автора, если оно есть
 
-        Returns:
-            None
+def write(path, header, created, source, author=''):
+    """ Пишет в файл отчёта строку с данными в кодировке windows-1251
 
-        """
-        result = [
-            path,
-            author,
-            "",
-            "",
-            header,
-            date.strftime(created, "%d.%m.%Y"),
-            self.SPHERE,
-            "",
-            "",
-            "",
-            "",
-            self.STYLE,
-            self.AUDIENCE_AGE,
-            self.AUDIENCE_LEVEL,
-            self.AUDIENCE_SIZE,
-            source,
-            self.PUBLICATION,
-            "",
-            created.year,
-            self.MEDIUM,
-            self.COUNTRY,
-            self.REGION,
-            self.LANGUAGE,
-        ]
+    Args:
+        path (unicode):  путь к файлу со статьёй
+        header (unicode): название статьи
+        created (datetime.date): дата публикации
+        source (unicode): URL, откуда статья была скачана
+        author (unicode): имя автора, если оно есть
 
-        self.report.writerow(result)
-        self.report_file.flush()
+    Returns:
+        None
+
+    """
+    result = [
+        path,
+        author,
+        "",
+        "",
+        header,
+        date.strftime(created, "%d.%m.%Y"),
+        SPHERE,
+        "",
+        "",
+        "",
+        "",
+        STYLE,
+        AUDIENCE_AGE,
+        AUDIENCE_LEVEL,
+        AUDIENCE_SIZE,
+        source,
+        PUBLICATION,
+        "",
+        created.year,
+        MEDIUM,
+        COUNTRY,
+        REGION,
+        LANGUAGE,
+    ]
+
+    report.writerow(result)
+    report_file.flush()
