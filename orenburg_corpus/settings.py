9e55867485e042ca873c83a1e3b10eb21ca75168
anastasiaromane <anastasiaromane@gmail.com>
2016-09-19 21:34:14 +0300

    dublicate files deleted

--- orenburg_corpus/settings.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-
-REPORT_FILE = '/Users/anastasiaromanova/Documents/programming_with_python/orenburg_corpus/report.csv'
-VISITED_LINKS = '/Users/anastasiaromanova/Documents/programming_with_python/orenburg_corpus/visited_links.txt'
-RESULT_DIR = '/Users/anastasiaromanova/Documents/programming_with_python/orenburg_corpus/result'
-ARCHIVE_LINK = 'http://vecherniyorenburg.ru/arkhiv/'
